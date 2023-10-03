7be5e26d5a3ff2d58acff04350336594734f8f43
LucasBondeDeSouza <111398229+LucasBondeDeSouza@users.noreply.github.com>
2023-10-03 20:22:53 -0300

    atualização

--- html/..js/script.js
+++ html/..js/script.js
@@ -1,37 +1,31 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const numero = document.getElementById('numero');
-    const resultado = document.getElementById('res');
-    const gerarBotao = document.getElementById('gerarTabuada');
-    const limparBotao = document.getElementById('limpar');
+const numero = document.getElementById('numero')
+const resultado = document.getElementById('res')
+const gerarBotao = document.getElementById('gerarTabuada');
 
-    gerarBotao.addEventListener('click', gerarTabuada);
-    limparBotao.addEventListener('click', limpar);
+function gerarTabuada() {
 
-    function gerarTabuada(event) {
-        event.preventDefault(); // Prevenir o comportamento padrão do formulário, se houver
+    if (numero.value.length == 0) {
+        alert("Digite um Número")
+        numero.value = ''
 
-        if (numero.value.length == 0) {
-            alert("Digite um Número");
-            numero.value = '';
-        } else if (numero.value <= 0) {
-            alert("Digite um número maior que 0");
-            numero.value = '';
-        } else {
-            resultado.innerHTML = ''; // Limpar resultados anteriores
-            for (let i = 1; i <= 10; i++) {
-                resultado.innerHTML += `${numero.value} X ${i} = <strong>${numero.value * i}</strong><br><br>`;
-            }
-            gerarBotao.style.display = 'none';
-            numero.disabled = true; // Desabilitar o input
+    } else if (numero.value <= 0) {
+        alert("Digite um número maior que 0")
+        numero.value = ''
+
+    } else {
+        for (let i = 1; i <= 10; i++) {
+            resultado.innerHTML += `${numero.value} X ${i} = <strong>${numero.value * i}</strong><br><br>`
         }
-    }
 
-    function limpar(event) {
-        event.preventDefault(); // Prevenir o comportamento padrão do formulário, se houver
-
-        gerarBotao.style.display = 'block';
-        numero.value = '';
-        resultado.innerHTML = '';
-        numero.disabled = false; // Habilitar o input novamente
+        gerarBotao.style.display = 'none'
+        numero.style.background = 'white'
+        numero.disabled = true
     }
-})
+}
+
+function limpar() {
+    gerarBotao.style.display = 'block'
+    numero.value = ''
+    resultado.innerHTML = ''
+    numero.disabled = false
+}
